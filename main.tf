4202bc0607715c8dcb464061c953de3170e9bfb1
smvijay <vj.smit97@gmail.com>
2023-07-18 03:44:13 -0400

    creating subnet

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
 resource "aws_vpc" "main" {
   cidr_block =var.cidr_block
-  enable_dns_support = true
+  tags = merge ({
+    Name = "${var.env}-vpc"
+  },
+    var.tags)
+}
+
+resource "aws_subnet" "main" {
+  count = length(var.web_subnet_cidr_block)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.web_subnet_cidr_block, count.index )
+
+  tags = merge ({
+    Name = "${var.env}-web-subnet"
+  },
+    var.tags)
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,4 @@
 variable "cidr_block" {}
+variable "tags" {}
+variable "env" {}
+variable "web_subnet_cidr_block" {}
